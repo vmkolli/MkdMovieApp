0f65ade2d0b8f4f8985c06fcbf898583fc289278
vinkol <vinkol@yahoo-inc.com>
2015-01-21 22:20:43 -0800

    Initial Commit

---
+++ MkdMovieListApp/MKDViewController.m
@@ -0,0 +1,29 @@
+//
+//  MKDViewController.m
+//  MkdMovieListApp
+//
+//  Created by Vinayakumar Kolli on 1/21/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import "MKDViewController.h"
+
+@interface MKDViewController ()
+
+@end
+
+@implementation MKDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ MkdMovieListApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MkdMovieListApp
+//
+//  Created by Vinayakumar Kolli on 1/21/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MKDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MKDAppDelegate class]));
+    }
+}
